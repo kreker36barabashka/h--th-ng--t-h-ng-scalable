d016982a20ab7d13ac501578fc50022a0f9f7349
kreker36barabashka <tackabiog@hotmail.com>
2025-10-06 13:13:16 -0500

    feat: initial commit for server/index.js

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());   
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+  .catch(err => console.error(err));
